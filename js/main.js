33c5cbc3b0e663efdb5dd36414b6b4cc73cfeb52
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-06-03 22:28:21 +0300

    fairy-tail and our-trip and hotel sections were added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+    $('.fairy-tail__slider, .our-trip__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-right.svg"></button>',
+        autoplay: true,
+        fade: true
+    });
 });
